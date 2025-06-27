ab18e224ca744698b2f920b0314e6426f60d1078
Carri1x <alvarocarrion.git@gmail.com>
2025-06-27 13:01:55 +0200

    app.routes.ts agregado

--- front-end/clientes-web/src/app/app.routes.ts
+++ front-end/clientes-web/src/app/app.routes.ts
@@ -1,7 +1,16 @@
 import { Routes } from '@angular/router';
 import { Login } from './components/login/login';
+import { ClientesList } from './components/clientes/clientes-list/clientes-list';
+import { MainLayout } from './layout/main-layout/main-layout';
+import { AgregarCliente } from './components/clientes/agregar-cliente/agregar-cliente';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'home/clientes', pathMatch: 'full' },
   { path: 'login', component: Login },
+  { path: 'home' , component: MainLayout,
+    children: [
+      {path: 'clientes', component: ClientesList},
+      {path: 'agregar-cliente', component: AgregarCliente}
+    ]
+  }
 ];
